f3cd8e94a365aec4309760adf1d2b06cb63ac369
JACKSON WESONGA <120163285+WesongaJackson@users.noreply.github.com>
2023-10-24 13:45:10 +0300

    Update settings.py
    change email settings

--- scr/settings.py
+++ scr/settings.py
@@ -130,6 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #email
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'jacknan598@gmail.com'
-EMAIL_HOST_PASSWORD = 'tloe jpqx tsqw jfot'
+EMAIL_HOST_USER = 'your email'
+EMAIL_HOST_PASSWORD = '##################'
 EMAIL_PORT = 587
